03714c0b7c6f9b5f4bc366dc9c64ee14d525291b
Abhiram Ampabathina <abhiram.ampabathina@oracle.com>
2025-12-03 22:11:02 -0600

    Add goodbye function to hello.py

--- hello.py
+++ hello.py
@@ -1,5 +1,9 @@
 def greet(name):
     return f'Hello, {name}!'
 
+def goodbye(name):
+    return f'Goodbye, {name}!'
+
 if __name__ == '__main__':
     print(greet('World'))
+    print(goodbye('World'))
